4e19cb609385b74c92a4cd1888a302337f018031
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 12:29:36 -0600

    changes to lastLetterOfLastName

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// this lesson taught us an easy way to find the last index of a string
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
