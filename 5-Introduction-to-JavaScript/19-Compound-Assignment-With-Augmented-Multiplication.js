db757b14966a4a860468c396da7382701e344c8b
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 09:15:58 -0600

    changed to augmented multiplication

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,10 @@
+// in this lesson of freeCodeCamp I learned how to use augmented multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
