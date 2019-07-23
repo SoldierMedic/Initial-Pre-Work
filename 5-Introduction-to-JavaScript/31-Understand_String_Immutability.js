3a46f99ff65c408acc920dfc9a71966775fde352
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 12:11:59 -0600

    changes to myStr

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+ // in this lesson I learned that string variables are immuteable, meaning they cannnot be altered at a single index.
+ // Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
