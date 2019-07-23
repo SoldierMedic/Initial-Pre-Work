19fa70e3950a28a1bb7b28092826598767dee831
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 09:55:21 -0600

    changes to myStr

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+// in this lesson of freeCodeCamp I learned how to place literal quotes inside of a string using the \
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
