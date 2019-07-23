6635efddf6876a95d8b6627a4041cfbec6203663
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 10:14:02 -0600

    changes to myStr

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+// in this lesson I learned that you can use the += operator on strings similarly to how it's used with numbers.
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
