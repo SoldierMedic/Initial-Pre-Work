c0c220bc02cd897c8911ce6050a5680d1f7694e7
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 10:54:51 -0600

    appends someAdjective to myStr

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+// in this lesson I appended a string to the end of another string
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fun";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
