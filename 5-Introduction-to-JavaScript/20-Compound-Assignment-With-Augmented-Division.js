19791ca92211040f39e49ea723860fc96197e70b
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 09:17:16 -0600

    changed to augmented division

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,10 @@
+// in this lesson of freeCodeCampI learned how to use augmented division
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
