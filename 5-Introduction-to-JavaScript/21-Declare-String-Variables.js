777612b95fd124cf0e6bc05cb88b0344cd06a957
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 09:20:08 -0600

    adds myFirstName and myLastName

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// in this lesson I learned how to create string variables
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Marco";
+var myLastName = "Rojas";
