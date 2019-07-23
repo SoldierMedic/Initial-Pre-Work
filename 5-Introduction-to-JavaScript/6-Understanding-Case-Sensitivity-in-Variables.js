6764f6e943a172e24e6a283ad6b5b185c14c106f
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 08:59:59 -0600

    changed to camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+// in this lesson I learned that its recommended to
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
