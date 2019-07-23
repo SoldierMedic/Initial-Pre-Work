590873bff12950eaac9379e77244bbd9d3482a6d
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 12:28:02 -0600

    changes to thirdLetterOfLastName

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// in this lesson of freeCodeCamp they taught that indexes start at num 0 instead of 1 in JS
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
