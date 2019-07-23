45f83cf70ba946cd0d94e1bbdc8e5e5a89341dd3
SoldierMedic <pv2rojas@gmail.com>
2019-07-23 11:03:05 -0600

    changes to firstLetterOfLastName

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,13 @@
+// in this lesson I used bracket notation to find  the string located at index 0 in lastName then set that value to firstLetterOfLastName
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
