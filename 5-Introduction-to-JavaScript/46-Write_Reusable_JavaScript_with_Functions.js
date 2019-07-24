b1402b9b4cc5a1f67263e3183f6596658698bc2a
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 09:57:26 -0600

    adds reusableFunction

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,13 @@
+// in this lesson we created a function that can be called on at any time to print "Hi World" to the console
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+  console.log("Hi World")
+}
+reusableFunction();
