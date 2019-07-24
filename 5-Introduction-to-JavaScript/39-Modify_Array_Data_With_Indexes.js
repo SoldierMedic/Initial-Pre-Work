661c2a53c706f1f7e0aef158da533b9afe2b2589
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 09:08:09 -0600

    changes to myArray

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,11 @@
+// in this lesson we changed the value inside an array by calling the index.
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+myArray[0]=45;
