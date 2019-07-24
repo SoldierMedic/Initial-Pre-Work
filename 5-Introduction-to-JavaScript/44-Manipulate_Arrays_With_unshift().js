41cd222737b553bf494372267fe20d549a6f080e
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 09:37:53 -0600

    changes first value of myArray

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,14 @@
+// in this lesson I learned about the unshift function which allows me to add values to the front of an array.
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+
+myArray.unshift(["Paul", 35]);
