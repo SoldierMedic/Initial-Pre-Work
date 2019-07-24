3d2887608f3069de747fd22523c19449a77f7901
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 09:06:29 -0600

    adds myData

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+// this lesson we made a variable and set it's value equal to the value of the first index in myArray
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
