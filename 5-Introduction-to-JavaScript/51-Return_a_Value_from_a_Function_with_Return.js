5e619124cc3d583ab2e19c787ca4b37d05f1824a
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 10:34:54 -0600

    adds timeFive

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+// in this lesson they have begun explaining the uses of returning values from functions.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+
+function timesFive(num){
+  return num * 5;
+}
+console.log(minusSeven(10));
