37f97ef00a02ad4d54f5d0a344e55e9952f5215a
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 09:03:55 -0600

    changes myArray to multidimensional

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+// in this lesson I learned that multidimensional arrays are similar in JS as other languages
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Bulls", 23],["White Sox" , 45] ];
