a44acd677f92777af2fc66ee311175b8753135be
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 09:53:02 -0600

    fills myList

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,6 @@
+// in this lesson we made a shopping list multi-dimensional array. I organized it in a way that used to make it easier for me to understand where rows vs columns are in a multi-dimensional array.
+var myList =[["Chocolate Bar", 15],
+             ["Fruit Cup", 5],
+             ["Water", 100],
+             ["Soda" , 50],
+             ["Juice", 20]];
