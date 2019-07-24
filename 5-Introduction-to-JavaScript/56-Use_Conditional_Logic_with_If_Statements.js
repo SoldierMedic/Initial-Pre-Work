4bd24369df0908d5fd8f4bac3686a74c1f00c6fd
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 16:40:07 -0600

    changes to trueOrFalse

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,25 @@
+// in this lesson of freeCodeCamp I learned that JS has if statements also. I'm guessing it will also have loops etc.
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+    if (wasThatTrue){
+       return "Yes, that was true";
+    }
+    return "No, that was false";
+
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
