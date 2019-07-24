085cabe4477a4ff369085552585ede5b1db9d6b7
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 09:35:13 -0600

    removes values from myArray

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,13 @@
+// in this lesson I learned about the shift function. It is very similar to the pop function except it will remove from the front of the array instead of the back
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray;
+
+removedFromMyArray = myArray.shift()
