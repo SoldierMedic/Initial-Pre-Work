947355ddabef7c4caa9a4d8d7adc7a5837196bfa
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 22:14:16 -0600

    changes to testElseIf

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,16 @@
+// this lesson had us change a set of if statements if, else if, and else statements.
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else{
+  return "Between 5 and 10";
+  }
+}
+
+// Change this value to test
+testElseIf(7);
