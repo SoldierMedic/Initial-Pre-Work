e59312f8fea21ed7a39791eb3c4365e551a84057
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 18:43:37 -0600

    changes to testNotEqual

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,11 @@
+// in this lesosn I learned about the inequality operator of JS. It also utilizes the same method as the equality operator.
+// Setup
+function testNotEqual(val) {
+  if (val!=99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
