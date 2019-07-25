a3c0ed979752d71097a857f6aed5861dae45328e
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 14:05:55 -0600

    parsing string

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,6 @@
+// in this lesson I learned about the parseInt function of JS. It allows me to create an int from a string value.
+function convertToInteger(str) {
+    return parseInt(str);
+}
+
+convertToInteger("56");
