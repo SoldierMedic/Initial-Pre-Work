5df878d87444440ce053354c15976b43c14b86b1
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 14:01:30 -0600

    changes to return statement

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,9 @@
+// in this lesson I was instructed to make a random number within a certain whole number range.
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() *10);
+}
