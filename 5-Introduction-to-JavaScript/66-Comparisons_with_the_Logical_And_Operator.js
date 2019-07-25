36537e3fa72d91a6c323438cdbf9bb9ac762c499
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 18:58:23 -0600

    changes to testLogicalAnd

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,16 @@
+// in this lesson they had us write a fuction that would return a true value if the arguement that it's being passed is <=50 && >=25
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val<=50 && val >=25) {
+
+      return "Yes";
+
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
