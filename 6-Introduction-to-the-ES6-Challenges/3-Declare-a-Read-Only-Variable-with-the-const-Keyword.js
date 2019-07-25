a194699790138a088d08d77e7ec417d79a1ab3db
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 14:29:27 -0600

    changes to keywords in printManyTimes

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,15 @@
+// in this lesson I learned ES6 allows you to use the const keyword.
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
