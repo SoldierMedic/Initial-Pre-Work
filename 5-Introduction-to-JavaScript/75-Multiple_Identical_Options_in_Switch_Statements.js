4590d9c0da4e008cdda411735f9278496e1f94e8
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 22:55:55 -0600

    changes to sequentialSizes

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,29 @@
+// in this lesson I learned that you are able to set more then one case to a result. This helps reduce redundancy
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
 
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
