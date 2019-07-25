25b730efefb8459607959ae5fbd613742ddddbc3
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 11:29:15 -0600

    adds reverse for loop

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,14 @@
+// in this lesson I was instructed to create a for loop to iterate backwards through odd numbers and push them to an array.
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+// Only change code below this line.
