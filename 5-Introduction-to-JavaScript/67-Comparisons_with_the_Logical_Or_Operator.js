fcc26ad09649eeb2123bc1d65f91e962000b3cc9
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 19:02:05 -0600

    changes to testLogicalOr

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,13 @@
+// in this lesosn we begin working with the Or operator that can be called using ||
+function testLogicalOr(val) {
+// Only change code below this line
+
+if (val<10 || val >20) {
+  return "Outside";
+}
+// Only change code above this line
+return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
