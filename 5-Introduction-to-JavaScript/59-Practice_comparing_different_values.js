d880b4c3fab8773d354fcb9b574e16c702abbc7e
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 18:14:15 -0600

    changes to compareEquality

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,11 @@
+// in this lesson I used the Type Coercsion to compare two different data types
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
