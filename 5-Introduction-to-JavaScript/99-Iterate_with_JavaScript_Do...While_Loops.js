7bb3363f8c00296a85278eb7cedb993b0f0fc011
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 13:48:34 -0600

    changed to a do while

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,11 @@
+// in this lesson I was instructed to change a while loop into a do - while loop.
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do  {
+  myArray.push(i);
+  i++;
+}while (i < 5);
