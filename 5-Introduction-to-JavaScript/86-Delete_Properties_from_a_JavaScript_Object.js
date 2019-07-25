74654eb8f10317f3cda00f8b7ac290d5bb5d98a0
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 00:42:59 -0600

    deletes tails from myDog object

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,24 @@
+// in this lesson I learned that there is a way to delete properties from an object in JS.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+
+delete myDog.tails;
