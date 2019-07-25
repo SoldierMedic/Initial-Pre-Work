b38359e3641a850442633d2322fe2bcbb40b90a9
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 18:47:43 -0600

    changes to testGreaterThan

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,15 @@
+// in this lesson I used the greater then operator to create two conditional statements.
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val>10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
