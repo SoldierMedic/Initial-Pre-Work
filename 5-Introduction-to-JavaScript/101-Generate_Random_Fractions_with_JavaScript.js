1ee1db07eda62225094387b4466a4b702e100e7f
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 13:59:15 -0600

    changes to return statement

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+// in this lesson I was instructed to change a return statement to return Math.random
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
