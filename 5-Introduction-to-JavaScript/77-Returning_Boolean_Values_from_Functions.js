c8d2834011fe4582f52ba7d9bfe160514cb39f3b
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 23:23:54 -0600

    changes to isLess

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+// in this lesson I used the <= operator to return a boolean from a function.
+function isLess(a, b) {
+  // Fix this code
+  return a<=b;
+}
+
+// Change these values to test
+isLess(10, 15);
