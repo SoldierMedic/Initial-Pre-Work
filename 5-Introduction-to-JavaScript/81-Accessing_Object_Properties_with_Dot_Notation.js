c7e76efc8393583aa1a2db2c3b90804eae48239a
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 00:07:25 -0600

    fixed object refernces

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+// in this lesson I accessed an objects properties using the dot notation that is very similar to Java.
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
