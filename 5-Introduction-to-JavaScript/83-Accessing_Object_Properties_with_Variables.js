4e475b9f91844a71490e5107a588ea7f77ba77e8
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 00:23:01 -0600

    changes to player and playerNumber vars

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,12 @@
+// in this lesson I learned how to set the value of a variable equal to the reference of an objects property.
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
