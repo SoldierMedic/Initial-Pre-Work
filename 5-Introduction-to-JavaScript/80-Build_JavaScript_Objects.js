67c3c694a32492e86d6cc44d09fcca9d46777f55
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 00:01:43 -0600

    changes myDog

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+// in this lesson I was intstructed to create an object in JS. I learned that JS will auto type cast all properties to strings if not specified.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  name: "Oreo",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything"]
+
+
+
+
+};
