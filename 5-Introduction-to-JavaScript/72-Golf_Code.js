5f8b139cbba5227858300047469cc4bd6817913d
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 22:37:06 -0600

    changes in golfScore

---
+++ 5-Introduction-to-JavaScript/72-Golf_Code.js
@@ -0,0 +1,30 @@
+// in this lesson of freeCodeCamp we were instructed to create a program that would keep track of golf stats. This is what I have come up with.
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes ==1){
+    return names[0];
+  }
+  else if (strokes<=par-2){
+    return names[1];
+  }
+  else if (strokes ==par-1){
+    return names[2];
+  }
+  else if (strokes==par){
+    return names[3];
+  }
+  else if (strokes == par+1){
+    return names[4];
+  }
+  else if (strokes== par +2){
+    return names[5];
+  }
+  else {
+    return names[6];
+    }
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(5, 4);
