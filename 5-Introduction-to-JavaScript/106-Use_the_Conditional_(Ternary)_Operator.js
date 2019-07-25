c32e9e56f40b3d7bf86a5964b7fdf764f309716b
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 14:13:25 -0600

    changes to checkEqual

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,6 @@
+// in this lesson I learned the JS version of the Ternary operator. It allows us to easily make simple conditional based statements
+function checkEqual(a, b) {
+  return a==b ? true:false;
+}
+
+checkEqual(1, 2);
