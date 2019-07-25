18fb9bc2719012c0e6edfec8a791bbd1a71d74cb
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 09:01:18 -0600

    adds instance of myMusic

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,24 @@
+// in this lesson of freeCodeCamp I learned how to create multiple instances of an object.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+   "artist": "The Beatles",
+   "title": "Strawberry Fields Forever",
+   "release_year": 1967,
+   "formats": [
+     "CD",
+     "8T",
+     "LP"
+   ],
+ }
+];
