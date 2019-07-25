52b4972d771560c6c62698abb9d53494884d5db5
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 14:19:02 -0600

    finished checkSign

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,6 @@
+// in this lesson I learned how to create multiple if statements inside the ternary property
+function checkSign(num) {
+  return (num>0) ? "positive" : (num ===0) ? "zero" : "negative";
+}
+
+checkSign(10);
