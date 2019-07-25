b0444f00e3bea5cb9e76484f5773dcfb7341b854
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 22:37:55 -0600

    adds switch statement to caseInSwitch

---
+++ 5-Introduction-to-JavaScript/73-Selecting_from_Many_Options_with_Switch_Statements.js
@@ -0,0 +1,26 @@
+// in this lesson of freeCodeCamp I was instructed to create a set of switch statements that would return different strings.
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val){
+    case 1:
+      answer="alpha";
+      break;
+    case 2:
+      answer="beta";
+      break;
+    case 3:
+      answer="gamma";
+      break;
+    case 4:
+      answer="delta";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
