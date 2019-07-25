75dc4c83cbaadc5f3aa273545b0ec643b2c038d7
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 14:39:56 -0600

    changes to magic

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,5 @@
+// in this lesson I learned ES6 allows us to create nameless functions which is useful in the case we won't need to reuse said function.
+const magic = () => {
+  "use strict";
+  return new Date();
+};
