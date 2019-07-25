d0c28772310f2ff9f0fac27335fe135992be98d0
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 00:25:00 -0600

    changes myDog name

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,21 @@
+// in this lesson I was instructed to change an objects property.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name = "Happy Coder";
+
+// Only change code below this line.
