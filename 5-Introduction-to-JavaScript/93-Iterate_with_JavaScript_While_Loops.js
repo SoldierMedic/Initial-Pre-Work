6efa2fe63cfd73b30356af8c43213882bf696c86
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 11:22:19 -0600

    adds while loop

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,10 @@
+// in this lesson I realized how easy arrays are in JS. The push property is a very useful feature that works well in conjunction with while loops
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while (i<5){
+    myArray.push(i);
+    i++;
+}
