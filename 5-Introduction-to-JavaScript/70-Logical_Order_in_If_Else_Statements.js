3dd9f3a7af28af373b12400f1da3b4e314e8adb2
SoldierMedic <pv2rojas@gmail.com>
2019-07-24 22:22:38 -0600

    changes to orderMyLogic

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,13 @@
+// in this lesson in freeCodeCamp we were instructed to rearrange an if, else if, else statement so the logic operators would properly work.
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
