b902073c12a5d327a230b3f68b347908ad92ff0e
SoldierMedic <pv2rojas@gmail.com>
2019-07-25 14:27:04 -0600

    changes to checkScope

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+// in this lesson I learned that the let declaration will not declare a global variable like the var keyword does.
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
