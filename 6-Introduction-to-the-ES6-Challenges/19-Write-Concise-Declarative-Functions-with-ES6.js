3e9cc48cf6cd549c56fa292696c2f6c0e4da093b
SoldierMedic <pv2rojas@gmail.com>
2019-07-26 10:58:11 -0600

    changes to setGear function

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+// this lesson taught me that ES6 allows me to create functions in a similar way to Java.
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
